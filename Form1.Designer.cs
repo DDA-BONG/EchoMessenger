4a3c006e71f62b903a92557fa6d0f5929bffe7fc
형규 윤 <yhk8835@naver.com>
2026-03-19 12:06:10 +0900

    과제1 코딩과 테스트 완료 ListBox를 중앙에 TextBox를 그 아래 배치 전송버튼을 누르면 TextBox에 있는 내용이 ListBox로 올라가고 TextBox에 있는 내용은 Clear됨

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,80 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblEchoMessenger = new System.Windows.Forms.Label();
+            this.lstChatting = new System.Windows.Forms.ListBox();
+            this.txtTypeHere = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblEchoMessenger
+            // 
+            this.lblEchoMessenger.AutoSize = true;
+            this.lblEchoMessenger.Font = new System.Drawing.Font("휴먼둥근헤드라인", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblEchoMessenger.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.lblEchoMessenger.Location = new System.Drawing.Point(12, 9);
+            this.lblEchoMessenger.Name = "lblEchoMessenger";
+            this.lblEchoMessenger.Size = new System.Drawing.Size(457, 45);
+            this.lblEchoMessenger.TabIndex = 0;
+            this.lblEchoMessenger.Text = "Echo Messenger";
+            this.lblEchoMessenger.Click += new System.EventHandler(this.lblEchoBox_Click);
+            // 
+            // lstChatting
+            // 
+            this.lstChatting.Font = new System.Drawing.Font("굴림", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lstChatting.FormattingEnabled = true;
+            this.lstChatting.ItemHeight = 23;
+            this.lstChatting.Location = new System.Drawing.Point(20, 70);
+            this.lstChatting.Name = "lstChatting";
+            this.lstChatting.Size = new System.Drawing.Size(756, 234);
+            this.lstChatting.TabIndex = 1;
+            this.lstChatting.SelectedIndexChanged += new System.EventHandler(this.lstChatting_SelectedIndexChanged);
+            // 
+            // txtTypeHere
+            // 
+            this.txtTypeHere.Font = new System.Drawing.Font("굴림", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.txtTypeHere.Location = new System.Drawing.Point(20, 362);
+            this.txtTypeHere.Name = "txtTypeHere";
+            this.txtTypeHere.Size = new System.Drawing.Size(562, 30);
+            this.txtTypeHere.TabIndex = 2;
+            this.txtTypeHere.TextChanged += new System.EventHandler(this.txtTypeHere_TextChanged);
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.Color.Gold;
+            this.btnSend.Font = new System.Drawing.Font("휴먼엑스포", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnSend.Location = new System.Drawing.Point(605, 339);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(171, 74);
+            this.btnSend.TabIndex = 3;
+            this.btnSend.Text = "전송";
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.MistyRose;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtTypeHere);
+            this.Controls.Add(this.lstChatting);
+            this.Controls.Add(this.lblEchoMessenger);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblEchoMessenger;
+        private System.Windows.Forms.ListBox lstChatting;
+        private System.Windows.Forms.TextBox txtTypeHere;
+        private System.Windows.Forms.Button btnSend;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.Generic;    
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading.Tasks;       
 using System.Windows.Forms;
 
 namespace EchoMessenger
@@ -16,5 +17,35 @@ namespace EchoMessenger
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblEchoBox_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lstChatting_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        string MyMessage;
+        private void txtTypeHere_TextChanged(object sender, EventArgs e)
+        {
+            MyMessage = txtTypeHere.Text;
+            
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            lstChatting.Items.Add(MyMessage);
+            txtTypeHere.Clear();
+        }
+
+        
+        }
     }
-}
+
